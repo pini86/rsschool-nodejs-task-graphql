b81906846175340bb06e1603d2ec609e6ca9b880
Mikhail Druzhinin <pini86@mail.ru>
2023-01-29 20:31:14 +0300

    feat: add depth limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,17 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
-import { graphql, GraphQLSchema } from 'graphql';
+import {
+  graphql,
+  GraphQLSchema,
+  parse,
+  validate,
+  ExecutionResult,
+} from 'graphql';
 import { rootQuery } from './query/rootQuery';
 import { rootMutation } from './mutation/rootMutation';
+import * as limitDepth from 'graphql-depth-limit';
+
+const LIMIT_DEPTH = 6;
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
@@ -16,14 +25,26 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
     },
     async function (request, reply) {
       const { query, variables } = request.body;
+      if (!query) {
+        return fastify.httpErrors.badRequest('Wrong query');
+      }
       const schema = new GraphQLSchema({
         query: await rootQuery(fastify),
         mutation: await rootMutation(fastify),
       });
+      const errors_validation = validate(schema, parse(query!), [
+        limitDepth(LIMIT_DEPTH),
+      ]);
 
+      if (errors_validation.length) {
+        return {
+          errors: errors_validation,
+          data: null,
+        } as ExecutionResult;
+      }
       return graphql({
         schema,
-        source: String(query!),
+        source: String(query),
         variableValues: variables,
       });
     }
